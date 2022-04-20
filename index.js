1af34ad2352348d903ba5911da98543bb5077835
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-04-20 18:06:11 -0400

    Employee and Manager Constructs

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
