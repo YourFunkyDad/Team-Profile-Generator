cdd16da5a4beb63e2e6c0d03a4b3063ca3b26761
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-04-20 17:25:15 -0400

    Init Files, Folders, Lib

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
